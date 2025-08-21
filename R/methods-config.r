df49d1ac1bb85d06cd04147b9bd442c3127f7632
madelattre <maud.delattre@inrae.fr>
2025-08-21 19:58:47 +0200

    refactor: update preprocessing methods to align with new DataC subclass

--- R/methods-config.r
+++ R/methods-config.r
@@ -12,81 +12,121 @@ setMethod(
   "prepare_data",
   signature(data = "dataC", model = "modelC"),
   function(data, model) {
-    if (length(model@index_select) == 0) {
-      # mle
+    if (length(model@phi_sel_idx) == 0) {
+      # MLE : pas de sélection spike and slab
+      # Toutes les covariables sont dans x_phi_insel
+      # x_forced_support se rapporte à tous les paramètres
 
-      v <- tv_v <- kron_tv_v <- NULL
+      x_phi_sel <- tx_x_phi_sel <- kron_tx_x_phi_sel <- NULL
 
-      if (is.null(data@w)) { # No fixed covariate, intercept only
-        data@w <- matrix(1, nrow = length(data@y_list), ncol = 1)
-        data@x <- lapply(
+      if (is.null(data@x_forced)) { # No fixed covariate, intercept only
+        x_phi_insel <- matrix(1, nrow = length(data@y_list), ncol = 1)
+        x_phi_insel_list <- lapply(
           seq_len(length(data@y_list)),
           function(i) {
             matrix(1, nrow = 1, ncol = 1)
           }
         )
       } else {
-        data@w <- cbind(1, data@w)
-        if (is.null(model@covariate_support)) {
-          supp <- matrix(1, ncol = model@q_phi, nrow = 1)
+        x_phi_insel <- cbind(1, data@x_forced)
+        if (is.null(model@x_forced_support)) {
+          supp <- matrix(1, ncol = model@phi_dim, nrow = 1)
         } else {
-          supp <- rbind(1, model@covariate_support)
+          supp <- rbind(1, model@x_forced_support)
         }
-        data@x <- from_v_to_x(
-          data@w, supp,
-          model@q_phi
+        x_phi_insel_list <- from_v_to_x(
+          x_phi_insel, supp,
+          model@phi_dim
         )
       }
-    } else if (length(model@index_select) == model@q_phi) {
+    } else if (length(model@phi_sel_idx) == model@phi_dim) {
       # Covariate matrix for the selection part of the model
-      data@v <- cbind(1, data@v)
-      data@tv_v <- t(data@v) %*% data@v
-      data@kron_tv_v <- as.matrix(Matrix::bdiag(replicate(
-        length(model@index_select),
-        data@tv_v,
+      # On traite ici le cas où toutes les composantes de phi
+      # comportent de la sélection
+      if (is.null(model@x_forced_covariate) ||
+        all(model@x_forced_covariate) == 0) {
+        # On n'ajoute pas de covariables de force
+        x_phi_sel <- cbind(1, data@x_sel)
+      } else {
+        # On ajoute des covariables de force
+        idx_forced <- which(apply(
+          model@x_forced_covariate, 1,
+          function(x) any(x == 1)
+        ))
+        x_phi_sel <- cbind(1, data@x_forced[, idx_forced], data@x_sel)
+      }
+
+      tx_x_phi_sel <- t(data@x_sel) %*% data@x_sel
+      kron_tx_x_phi_sel <- as.matrix(Matrix::bdiag(replicate(
+        model@phi_dim,
+        tx_x_phi_sel,
         simplify = FALSE
       )))
 
-      data@w <- NULL
-      data@x <- NULL
+      x_phi_insel <- NULL
+      x_phi_insel_list <- NULL
     } else {
-      # map
-
       # Covariate matrix for the selection part of the model
-      data@v <- cbind(1, data@v)
-      data@tv_v <- t(data@v) %*% data@v
-      data@kron_tv_v <- as.matrix(Matrix::bdiag(replicate(
-        length(model@index_select),
-        data@tv_v,
+      if (is.null(model@x_forced_covariate) ||
+        all(model@x_forced_covariate) == 0) {
+        # On n'ajoute pas de covariables de force
+        x_phi_sel <- cbind(1, data@x_sel)
+      } else {
+        # On ajoute des covariables de force
+        idx_forced <- which(apply(
+          model@x_forced_covariate[, model@phi_sel_idx], 1,
+          function(x) any(x == 1)
+        ))
+        x_phi_sel <- cbind(1, data@x_forced[, idx_forced], data@x_sel)
+      }
+
+      tx_x_phi_sel <- t(data@x_sel) %*% data@x_sel
+      kron_tx_x_phi_sel <- as.matrix(Matrix::bdiag(replicate(
+        model@phi_dim,
+        tx_x_phi_sel,
         simplify = FALSE
       )))
 
-      # Covariate matrix for the effects with no covariate selection
-      if (is.null(data@w)) { # No fixed covariate, intercept only
-        data@w <- matrix(1, nrow = length(data@y_list), ncol = 1)
-        data@x <- lapply(
+
+
+      idx_forced_insel <- which(apply(
+        model@x_forced_covariate[, -model@phi_sel_idx], 1,
+        function(x) any(x == 1)
+      ))
+
+      x_phi_insel <- data@x_forced[, idx_forced_insel]
+
+      if (length(idx_forced_insel) == 0) {
+        # Il y a des covariables de force dans les composantes de phi
+        # qui ne comportent pas de sélection
+        x_phi_insel_list <- from_v_to_x(
+          x_phi_insel,
+          model@x_forced_support[, -model@phi_sel_idx]
+        )
+      } else {
+        # Il n'y a pas de covariables de force dans les composantes de phi
+        # qui ne comportent pas de sélection
+        x_phi_insel_list <- lapply(
           seq_len(length(data@y_list)),
           function(i) {
             matrix(1, nrow = 1, ncol = 1)
           }
-        )
-      } else {
-        data@w <- cbind(1, data@w)
-        if (is.null(model@covariate_support)) {
-          supp <- matrix(1, nrow = model@q_phi -
-            length(model@index_select), ncol = 1)
-        } else {
-          supp <- rbind(1, model@covariate_support)
-        }
-        data@x <- from_v_to_x(
-          data@w, supp,
-          model@q_phi -
-            length(model@index_select)
         )
       }
     }
 
-    return(data)
+    data_alg <- new("dataAlgo",
+      y_list = data@y_list,
+      t_list = data@t_list,
+      x_sel = data@x_sel,
+      x_forced = data@x_forced,
+      x_phi_sel = x_phi_sel,
+      x_phi_insel = x_phi_insel,
+      tx_x_phi_sel = tx_x_phi_sel,
+      kron_tx_x_phi_sel = kron_tx_x_phi_sel,
+      x_phi_insel_list = x_phi_insel_list
+    )
+    return(data_alg)
   }
 )
 
@@ -108,14 +148,7 @@ setMethod(
       init@alpha <- rep(0.5, length(model@index_select))
     }
 
-    if ((length(model@index_select) != model@q_phi) &&
-      (!is.null(model@covariate_support))) {
-      index <- which(model@covariate_support == 0)
-      # if (init@beta_ldim[index] != 0) {
-      #   init@beta_ldim[index] <- 0
-      #   # Afficher un warning?
-      # }
-    }
+    # A compléter, initialisation de beta par défaut?
 
     return(init)
   }
@@ -132,15 +165,15 @@ setGeneric(
 
 setMethod(
   "prepare_hyper",
-  signature(hyper = "hyperC", data = "dataC", model = "modelC"),
+  signature(hyper = "hyperC", data = "dataAlgo", model = "modelC"),
   function(hyper, data, model) {
-    nbs <- length(model@index_select)
+    nbs <- length(model@phi_sel_idx)
 
     if (nbs == 0) { # mle
       hyper <- hyperC(NULL, NULL, NULL)
     } else { # map
 
-      p <- dim(data@v)[2] - 1
+      p <- dim(data@x_sel)[2] #- 1
 
       if (is.null(hyper@a)) {
         hyper@a <- rep(1, nbs)
@@ -167,12 +200,12 @@ setGeneric(
 setMethod(
   "make_config",
   signature(
-    data = "dataC", model = "modelC", tuning_algo = "tuningC",
+    data = "dataAlgo", model = "modelC", tuning_algo = "tuningC",
     init = "initC", hyperparam = "hyperC"
   ),
   function(data, model, tuning_algo, init, hyperparam) {
-    q_phi <- model@q_phi
-    index_select <- model@index_select
+    q_phi <- model@phi_dim
+    index_select <- model@phi_sel_idx
 
     if (length(index_select) == 0) {
       method <- "mle"
@@ -208,18 +241,22 @@ setMethod(
       ni = lengths(data@y_list),
       ntot = sum(lengths(data@y_list)),
       ti = data@t_list,
-      v = data@v,
-      tv_v = data@tv_v,
-      kron_tv_v = data@kron_tv_v,
-      w = data@w,
-      x = data@x,
-      pv = dim(data@v)[2] - 1,
-      pw = dim(data@w)[2] - 1,
+      v = data@x_phi_sel,
+      tv_v = data@tx_x_phi_sel,
+      kron_tv_v = data@kron_tx_x_phi_sel,
+      w = data@x_phi_insel,
+      x = data@x_phi_insel_list,
+      pv = dim(data@x_phi_sel)[2] - 1,
+      pw = dim(data@x_phi_insel)[2] - 1,
       g = model@model_func,
-      support = model@covariate_support, # est-ce qu'on a besoin de support?
-      supp_index = which(rbind(1, model@covariate_support) == 1),
-      index_fixed = model@index_fixed,
-      q_fixed = length(model@index_fixed),
+      support = model@x_forced_support[, -model@phi_sel_idx],
+      # est-ce qu'on a besoin de support? CHECK
+      supp_index = which(rbind(
+        1,
+        model@x_forced_support[, -model@phi_sel_idx]
+      ) == 1),
+      index_fixed = model@phi_fixed_idx,
+      q_fixed = length(model@phi_fixed_idx),
       # covariance_structure = model$covariance_structure, # to be removed?,
       niter = tuning_algo@niter,
       nburnin = tuning_algo@nburnin,
