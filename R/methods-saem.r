f223d347c54e2b6de56f97faa8d11ea6c39d724a
madelattre <maud.delattre@inrae.fr>
2025-08-26 19:50:46 +0200

    refactor: clarify function names in saem-functions and add roxygen docs

--- R/methods-saem.r
+++ R/methods-saem.r
@@ -33,27 +33,27 @@ setMethod(
 
     switch(case,
       map_full_select = {
-        state <- run_generic_saem(
+        state <- run_saem_full(
           config, state, sa_step_to_select, m_step_map_to_select, update_proposal_mh_to_select
         )
       },
       map_part_select_nofixed = {
-        state <- run_generic_saem(
+        state <- run_saem_full(
           config, state, sa_step_all, m_step_map_all, update_proposal_mh_all
         )
       },
       map_part_select_fixed = {
-        state <- run_fixed_saem(
+        state <- run_saem_with_fixed_effects(
           config, state, sa_step_all, m_step_map_all, update_proposal_mh_all
         )
       },
       mle_nofixed = {
-        state <- run_generic_saem(
+        state <- run_saem_full(
           config, state, sa_step_not_to_select, m_step_mle, update_proposal_mh_not_to_select
         )
       },
       mle_fixed = {
-        state <- run_fixed_saem(
+        state <- run_saem_with_fixed_effects(
           config, state, sa_step_not_to_select, m_step_mle, update_proposal_mh_not_to_select
         )
       }
