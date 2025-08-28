bd94fc70831ef98768c9126dd92fba615bc6dd73
madelattre <maud.delattre@inrae.fr>
2025-08-28 16:59:26 +0200

    chore: add utils-global.R to declare global variables and provide C++ stubs for NOT_CRAN

---
+++ R/utils-global.r
@@ -0,0 +1,19 @@
+# R/utils-global.R
+
+# Declare global variable names to suppress R CMD check notes
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(
+    c("iteration", "value", "crit")  # variables used in ggplot or data.table
+  )
+}
+
+# Create stubs for C++ functions dynamically generated at runtime, only for NOT_CRAN
+if (getRversion() >= "2.15.1") {
+  if (identical(Sys.getenv("NOT_CRAN"), "true")) {
+    g_vector_cpp <- function(...) stop("stub")
+    metropolis_vector_cpp <- function(...) stop("stub")
+    g_scalar_cpp <- function(...) stop("stub")
+    rmvnorm_cpp <- function(...) stop("stub")
+    logdmvnorm_cpp <- function(...) stop("stub")
+  }
+}
