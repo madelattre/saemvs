aad1f805e945813c4cc0266f31edda9abb60d31b
madelattre <maud.delattre@inrae.fr>
2025-10-29 18:49:28 +0100

    chore: :fire: remove obsolete unit tests related to deleted functions

--- tests/testthat/test_checks.r
+++ tests/testthat/test_checks.r
@@ -8,7 +8,7 @@ cov_valid <- matrix(c(2, 1, 1, 2), nrow = 2) # positive definite, symmetric
 cov_non_numeric <- matrix(c("a", "b", "c", "d"), 2, 2)
 cov_non_square <- matrix(c(1, 2, 3, 4, 5, 6), nrow = 2)
 cov_non_symmetric <- matrix(c(1, 2, 3, 4), nrow = 2)
-cov_not_pos_def <- matrix(c(1, 2, 2, 1), nrow = 2) # eigenvalues less or equal to zero
+cov_not_pos_def <- matrix(c(1, 2, 2, 1), nrow = 2) # eigenvalues less than zero
 
 # ---------------------------------------------
 # --- Tests for check_covariance() ---
@@ -44,23 +44,6 @@ test_that("check_covariance identifies positive definite matrices", {
   )
 })
 
-# ---------------------------------------------
-# --- Tests for check_beta_gamma() ---
-# ---------------------------------------------
-
-test_that("check_beta_gamma validates non-empty matrices", {
-  expect_error(check_beta_gamma(NULL, gamma_ns, q = 2, case = "ns"))
-  expect_error(check_beta_gamma(beta_ns, NULL, q = 2, case = "ns"))
-})
-
-# ---------------------------------------------
-# --- Tests for check_beta_support() ---
-# ---------------------------------------------
-
-test_that("check_beta_support checks support consistency", {
-  expect_error(check_beta_support(beta_ns, support_valid))
-  expect_error(check_beta_support(matrix(c(1, 2, 3, 4), nrow = 2), NULL))
-})
 
 # ---------------------------------------------
 # --- Tests for numeric slot checks ---
